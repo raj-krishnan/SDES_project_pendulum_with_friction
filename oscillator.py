8df383e5234e88e8d90e72edcb0adc0fbffa7a9c
Raj Krishnan <rajkrishnan1996@gmail.com>
2016-10-21 12:56:32 +0530

    Added oscillator class and tests

---
+++ oscillator.py
@@ -0,0 +1,53 @@
+import math
+import numpy
+from scipy import constants
+import scipy.integrate as integrate
+
+
+class Oscillator:
+    """
+    Variable: theta
+    First Derivative: omega
+    Second Derivative: alpha
+    """
+
+    def __init__(self, state=(5 * math.pi / 180, 0), c=1.0, radius=1.0):
+        allowed_types = [int, float]
+
+        if type(c) not in allowed_types \
+                or type(radius) not in allowed_types \
+                or type(state[0]) not in allowed_types \
+                or type(state[1]) not in allowed_types:
+            raise TypeError("Expected a numeric type")
+
+        if len(state) != 2:
+            raise TypeError("Expected list of length 2")
+
+        self.c = c
+        self.radius = radius
+        self.state = list(state)
+
+    def get_derivative(self, state, t):
+        omega = state[1]
+        alpha = (- self.c * state[1] - constants.g * state[0]) / self.radius
+        return [omega, alpha]
+
+    def update_state(self, time_step):
+        self.state = integrate.odeint(self.get_derivative,
+                                      self.state,
+                                      [0, time_step])[1]
+        return self.state
+
+    def get_trajectory(self, simulation_time=10.0, simulation_steps=500,
+                       initial_state=None):
+
+        if initial_state is not None:
+            self.state = initial_state
+
+        y = [numpy.asarray(self.state)]
+
+        for i in range(simulation_steps):
+            print("Iteration: " + str(i + 1))
+            self.update_state(simulation_time / simulation_steps)
+            y.append(self.state)
+        return y
---
+++ oscillator_tests.py
@@ -0,0 +1,24 @@
+from oscillator import Oscillator
+import unittest
+
+
+class TestOscillator(unittest.TestCase):
+    def test_flags_incorrect_arguments(self):
+        self.assertRaises(TypeError, Oscillator, "42", "answer", "everything")
+
+    def test_flags_incorrect_initialisation(self):
+        self.assertRaises(TypeError, Oscillator, [42, 42, 42], 42, 42)
+
+    def test_state_unchanged_after_zero_time(self):
+        oscillator = Oscillator()
+        self.assertListEqual(oscillator.state,
+                             list(oscillator.get_trajectory(0, 1)[0]))
+
+    def test_amplitude_decreases_when_friction_present(self):
+        oscillator = Oscillator(c=1.0, radius=1.0)
+        self.assertTrue(oscillator.state[0]
+                        > oscillator.get_trajectory(10.0, 500)[-1][0])
+
+
+if __name__ == '__main__':
+    unittest.main()
